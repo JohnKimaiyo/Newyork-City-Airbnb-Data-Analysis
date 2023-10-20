be04be3db1997e433a0bb63e61e9a391f5364034
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-10-20 11:41:14 +0300

    final commit

---
+++ Backend/sql/window functions/ny.sql
@@ -0,0 +1,38 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+-- windows functions with over ---
+
+
+-- Average price with OVER
+
+
+
+SELECT 
+[id]
+      ,[name]
+      ,[host_id]
+
+      ,[neighbourhood_group]
+      ,[neighbourhood]
+
+      ,AVG(price) OVER()
+      
+
+  FROM [dbo].[nybookings]
+
+  SELECT
+  [id],
+  [name],
+  [neighbourhood_group],
+  AVG(price) OVER() ,
+  MIN(price) OVER() ,
+  MAX(price) OVER()
+  FROM [dbo].[nybookings];
+
+    SELECT
+  [id],
+  [name],
+  [neighbourhood_group],
+  [price] ,
+  ROUND(AVG(price) OVER(),2) AS averageprcie,
+  ROUND((price - AVG(price) OVER()),2) AS diff_from_avg
+  FROM [dbo].[nybookings];
