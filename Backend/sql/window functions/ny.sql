8e1fde4ef8592b83b8283c55e5847065e1fcb5c3
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-10-20 11:51:15 +0300

    aveagre price

--- Backend/sql/window functions/ny.sql
+++ Backend/sql/window functions/ny.sql
@@ -36,3 +36,15 @@ SELECT
   ROUND(AVG(price) OVER(),2) AS averageprcie,
   ROUND((price - AVG(price) OVER()),2) AS diff_from_avg
   FROM [dbo].[nybookings];
+
+
+  -- percentage of avaergare price with Over()
+      SELECT
+  [id],
+  [name],
+  [neighbourhood_group],
+  [price] ,
+  ROUND(AVG(price) OVER(),2) AS averageprcie,
+  ROUND((price / AVG(price) OVER() * 100),2) AS percentage_of_avg_price
+  FROM [dbo].[nybookings];
+
